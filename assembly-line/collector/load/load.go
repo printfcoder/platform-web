00187790dfd0f84930b1cc1bc1e7f989b2086a8d
Shu Xian <i@shux.me>
2019-06-09 22:54:09 +0800

    load

---
+++ assembly-line/collector/load/load.go
@@ -0,0 +1,30 @@
+package load
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/micro-in-cn/platform-web/assembly-line/collector/db"
+	proto "github.com/micro-in-cn/platform-web/assembly-line/protobuf/go/load"
+	"github.com/micro/cli"
+	"github.com/micro/go-micro/server"
+)
+
+var (
+	o  *sql.DB
+	ds *loadStorage
+)
+
+type collector struct {
+}
+
+func (c *collector) PushLoadAvgStat(ctx context.Context, req *proto.LoadRequest, rsp *proto.LoadResponse) (err error) {
+	err = ds.saveLoadAvgStat(req.LoadAvgStat, req.IP, req.NodeName)
+	return
+}
+
+func Init(server server.Server, ctx *cli.Context) {
+	proto.RegisterLoadServiceHandler(server, new(collector))
+	o = db.GetPG()
+	ds = new(loadStorage)
+}
---
+++ assembly-line/collector/load/load_db.go
@@ -0,0 +1,14 @@
+package load
+
+import (
+	"database/sql"
+	proto "github.com/micro-in-cn/platform-web/assembly-line/protobuf/go/load"
+)
+
+type loadStorage struct {
+	db *sql.DB
+}
+
+func (c *loadStorage) saveLoadAvgStat(usageStat []*proto.LoadAvgStat, ip, nodeName string) (err error) {
+	return
+}
