6965e813e5f971f3061900e70498ec39735881c2
Shu Xian <i@shux.me>
2019-06-09 22:53:37 +0800

    format

--- assembly-line/collector/disk/disk_db.go
+++ assembly-line/collector/disk/disk_db.go
@@ -2,21 +2,21 @@ package disk
 
 import (
 	"database/sql"
-	"github.com/micro-in-cn/platform-web/assembly-line/protobuf/go/disk"
+	proto "github.com/micro-in-cn/platform-web/assembly-line/protobuf/go/disk"
 )
 
 type diskStorage struct {
 	db *sql.DB
 }
 
-func (c *diskStorage) saveDiskUsageStat(usageStat []*disk.UsageStat, ip, nodeName string) (err error) {
+func (c *diskStorage) saveDiskUsageStat(usageStat []*proto.UsageStat, ip, nodeName string) (err error) {
 	return
 }
 
-func (c *diskStorage) savePartitionStat(usageStat []*disk.PartitionStat, ip, nodeName string) (err error) {
+func (c *diskStorage) savePartitionStat(usageStat []*proto.PartitionStat, ip, nodeName string) (err error) {
 	return
 }
 
-func (c *diskStorage) saveIOCountersStat(usageStat []*disk.IOCountersStat, ip, nodeName string) (err error) {
+func (c *diskStorage) saveIOCountersStat(usageStat []*proto.IOCountersStat, ip, nodeName string) (err error) {
 	return
 }
