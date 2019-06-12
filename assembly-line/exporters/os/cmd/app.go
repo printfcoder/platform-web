58ef5b16cfee828f4e368c9f35d3a15db5561ba2
Shu Xian <i@shux.me>
2019-06-12 23:52:32 +0800

    config ing

--- assembly-line/exporters/os/cmd/app.go
+++ assembly-line/exporters/os/cmd/app.go
@@ -33,6 +33,11 @@ func init() {
 func (app *c) advFlags() {
 	app.Flags = append(app.Flags,
 		cli.StringFlag{
+			Name:   "config_file",
+			Usage:  "path to config file, but the configs in file are less weight than those passed on command",
+			EnvVar: "MICRO_WEB_PLATFORM_CONFIG_FILE",
+		},
+		cli.StringFlag{
 			Name:   "enable_cpu",
 			Usage:  "enables exporter to export CPU info",
 			EnvVar: "MICRO_WEB_PLATFORM_ENABLE_CPU",
